e91f202ae43c2dfea25e0fde791c6a685497509c
palsch <paul@pschwind.de>
2020-02-18 22:58:51 +0100

    add basic (buggy) setup of the npm package and some basic code

---
+++ SortableList.svelte
@@ -0,0 +1,47 @@
+<script>
+
+    import Sortable from 'sortablejs/modular/sortable.core.esm.js';
+
+    // every item is an object with a unique ID for identification
+    export let items = [];
+    export let idKey = "id"; // id attribute changeable if needed
+    export let ulClass = "";
+    export let liClass = "";
+    export let sortableOptions = {};
+    sortableOptions.store = sortableOptions.store || {
+        set: sortable => undefined // placeholder
+    };
+
+    function itemsToObject() {
+        // returns an object of items such that Object.values(items) == items
+        // and the keys are the IDs
+        let obj = {};
+        for (let item of items) {
+            obj[item[idKey]] = item;
+        }
+        return obj;
+    }
+
+    let _store_set = sortableOptions.store.set;
+    sortableOptions.store.set = (sortable) => {
+        let itemObj = itemsToObject();
+        items = sortable.toArray().map(id => itemObj[id]);
+        _store_set(sortable); // still call old set callback function
+    };
+    
+    let sortable;
+    let listElement;
+    $: if(listElement) sortable = Sortable.create(listElement, sortableOptions);
+
+</script>
+
+
+<ul bind:this={ listElement } class={ ulClass }>
+    {#each items as item}
+
+	<li class={ liClass } data-id={ item[idKey] }>
+        <slot { item }>{ item }</slot>
+    </li>
+
+    {/each}
+</ul>
---
+++ example/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+
+    // Normally, this would be import: `import SortableList from 'svelte-sortablejs';`
+    import SortableList from "../../SortableList.svelte";
+
+    let items = [1, 2, 3, 4].map(i => ({ identifierNumber: i, text: "item " + i }));
+
+    function addItem() {
+        if (items.length <= 0) items.push(1);
+        else items.push(items[items.length - 1] + 1);
+    }
+
+    function removeItem() {
+        items.pop();
+    }
+
+</script>
+
+<h1>svelte-sortablejs example</h1>
+
+<p>Try dragging them around!</p>
+
+<SortableList bind:items idKey="identifierNumber" let:item>
+    Hey, it's { item.text }!
+</SortableList>
+
+
+<button on:click={ addItem }>add item</button>
+<button on:click={ removeItem }>remove item</button>
