766df32b4684856a7952452da7475916eb6bda02
palsch <paul@pschwind.de>
2020-02-19 01:03:18 +0100

    demonstrate how to use styles so that svelte does not optimize them away

--- example/src/App.svelte
+++ example/src/App.svelte
@@ -1,6 +1,6 @@
 <script>
 
-    // Normally, this would be import: `import SortableList from 'svelte-sortablejs';`
+    // Normally, this would be import: `import SortableList from '@palsch/svelte-sortablejs';`
     import SortableList from "../../SortableList.svelte";
 
     let items = [1, 2, 3, 4].map(i => ({ identifierNumber: i, text: "item " + i }));
@@ -35,10 +35,23 @@
 
 <p>Try dragging them around!</p>
 
-<SortableList { sortableOptions } on:orderChanged={ itemOrderChanged } bind:items idKey="identifierNumber" let:item>
+<SortableList { sortableOptions } on:orderChanged={ itemOrderChanged } 
+        bind:items idKey="identifierNumber" let:item
+        liClass="largerFont">
     Hey, it's { item.text }!
 </SortableList>
 
 
 <button on:click={ addItem }>add item</button>
 <button on:click={ removeItem }>remove item</button>
+
+
+<style>
+
+    /* you need to use :global so that Svelte does not optimize away the in its view "unused" classes
+       see https://github.com/sveltejs/svelte/issues/2870 */
+    :global(.largerFont) {
+        font-size: 1.5rem;
+    }
+
+</style>
